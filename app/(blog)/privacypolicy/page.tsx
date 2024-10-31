a0d73fd7d216989c47c293c94ae10486f396f34c
Bradley Justice <bradleyjusticeca@gmail.com>
2024-10-30 22:32:48 -0400

    add privacy policy

---
+++ app/(blog)/privacypolicy/page.tsx
@@ -0,0 +1,20 @@
+
+
+export default function PrivacyPolicy() {
+
+    return (
+        <div className="page_main__SdEtU">
+            
+            <h1>Privacy Policy</h1>
+            <p>The management team has the following privacy policy (hereinafter referred to as "this policy") regarding the handling of user's personal information in the service provided on this application (hereinafter referred to as "this service").
+            To determine.</p>
+            <h2>Article 1 (Personal information)</h2>
+            <p>"Personal information" shall refer to "personal information" as defined in the Personal Information Protection Law, and is information about living individuals, such as profiles (including name, email address, and icons) and other information contained in the information.
+            Information that can identify a specific individual by description, etc., and information that can identify a specific individual (personal identification information) such as appearance.</p><h2>Article 2 (How to collect personal information)</h2><p>This service does not collect the data of the text to be translated or the translated text.
+            Such information is kept only within the app.
+            We collect the text to be translated and general access information other than the translated text and send it to the analysis service.<br/></p><h2>Article 3 (Handling of acquired personal information)</h2><p>The data sent to the analysis service will be handled for the purpose of providing and operating this service.
+            <br/>The data sent to the analysis service will not be sent to other companies or third-party tools.</p><h2>Article 4 (Purpose of collecting and using personal information)</h2><p>The purpose of collecting and using personal information by the management team shall be as set forth below.<br/>・For the provision and operation of this service <br/>Purpose incidental to the above purpose of use</p><h2>Article 5 (Change of purpose of use)</h2><p>The management team shall change the purpose of use of personal information only when it is reasonably recognized that the purpose of use is related to the purpose before the change.</p><h2>Article 6 (Provision of personal information to a third party)</h2><p>When the management team is requested to disclose personal information by the person, the management team will disclose it to the person without delay.<br/>However, if the disclosure falls under any of the following, we may not disclose all or part of it, and if we decide not to disclose it, we will notify you to that effect without delay.<br/>When there is a risk of harming the life, body, property or other rights and interests of the person or a third party.<br/>When there is a risk of significant hindrance to the proper execution of the operations of the management team<br/>In case of violating other laws and regulations<br/>Notwithstanding the provisions of the preceding paragraph, in principle, we will not disclose information other than personal information such as history information and characteristic information.<br/></p><h2>Article 7 (Change of privacy policy)</h2><p>The contents of this policy may be changed without notifying the user, except for laws and regulations and other matters specified otherwise in this policy.<br/>Unless otherwise specified by the management team, the changed privacy policy shall take effect from the time it is posted on this service.</p><h2>Article 8 (Inquiries)</h2><p>For inquiries regarding this policy, please contact the following.
+            <br/>Email address: bradleyjusticeca@gmail.com <br/>Enactment date: October 26, 2024</p>
+        </div>
+    )
+}
